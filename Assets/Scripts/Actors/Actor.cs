1ccf61d1310ce104eb1622a103952547cd65f0b6
damoch <damochramowicz260@gmail.com>
2018-11-04 19:09:06 +0100

    Hello git

---
+++ Assets/Scripts/Actors/Actor.cs
@@ -0,0 +1,149 @@
+﻿using Assets.Scripts.Enums;
+using UnityEngine;
+namespace Assets.Scripts.Actors
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Actor : MonoBehaviour
+    {
+        #region Private variables
+        [SerializeField]
+        private bool _isSelected;
+
+        [SerializeField]
+        private int _healthPoints;
+
+        [SerializeField]
+        private bool _isAlive;
+
+        [SerializeField]
+        private float _speed;
+
+        [SerializeField]
+        private string _name;
+
+        [SerializeField]
+        private Team _team;
+
+        private GameObject _actorDisplayer;
+        private Rigidbody2D _rigidbody2D;
+        private LineRenderer _lineRenderer;
+
+        #endregion
+
+        #region Accesors
+        public bool IsSelected
+        {
+            get
+            {
+                return _isSelected;
+            }
+
+            set
+            {
+                if (!_isAlive)
+                {
+                    return;
+                }
+                _isSelected = value;
+                _rigidbody2D.bodyType = _isSelected ? RigidbodyType2D.Dynamic : RigidbodyType2D.Kinematic;
+                _actorDisplayer.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+        }
+
+        public int HealthPoints
+        {
+            get
+            {
+                return _healthPoints;
+            }
+
+            set
+            {
+                if(value <= 0)
+                {
+                    KillActor();
+                }
+                _healthPoints = value;
+            }
+        }
+
+        private void KillActor()
+        {
+            _isAlive = false;
+            _isSelected = false;
+            _speed = 0;
+        }
+
+        public bool IsAlive
+        {
+            get
+            {
+                return _isAlive;
+            }
+        }
+
+        public Team Team
+        {
+            get
+            {
+                return _team;
+            }
+
+            set
+            {
+                _team = value;
+            }
+        }
+        #endregion
+
+        private void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _actorDisplayer = transform.GetChild(0).gameObject;
+            _lineRenderer = _actorDisplayer.GetComponent<LineRenderer>();
+
+            _rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
+            _isSelected = false;
+
+            if (_healthPoints > 0)
+            {
+                _isAlive = true;
+            }
+        }
+
+        public bool GetCommand(Commands command)
+        {
+            var spd = _speed * Time.deltaTime;
+            switch (command)
+            {
+                case Commands.Up:
+                    transform.Translate(Vector2.up * spd);
+                    break;
+                case Commands.Down:
+                    transform.Translate(Vector2.down * spd);
+                    break;
+                case Commands.Right:
+                    transform.Translate(Vector2.right * spd);
+                    break;
+                case Commands.Left:
+                    transform.Translate(Vector2.left * spd);
+                    break;
+                case Commands.Shoot:
+                    break;
+            }
+            return true;
+        }
+
+        public void LookAt(Vector2 position)
+        {
+            _actorDisplayer.transform.localRotation = Quaternion.Euler(0f, 0f, AngleBetweenTwoPoints(transform.position, position));
+            _lineRenderer.SetPosition(1, position);
+        }
+
+        private float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+        {
+            return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Controllers/PlayableActorController.cs
@@ -0,0 +1,73 @@
+﻿using Assets.Scripts.Actors;
+using Assets.Scripts.Enums;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Assets.Scripts.Controllers
+{
+    public class PlayableActorController : MonoBehaviour
+    {
+        #region Private variables
+        [SerializeField]
+        private List<Actor> _playableActors;
+
+        [SerializeField]
+        private KeyCode _upKey;
+
+        [SerializeField]
+        private KeyCode _downKey;
+
+        [SerializeField]
+        private KeyCode _leftKey;
+
+        [SerializeField]
+        private KeyCode _rightKey;
+
+        private Dictionary<KeyCode, Commands> _keyCodesToCommands;
+
+        #endregion
+
+
+        private void Start()
+        {
+            _keyCodesToCommands = new Dictionary<KeyCode, Commands>
+            {
+                { _upKey, Commands.Up },
+                { _downKey, Commands.Down },
+                { _leftKey, Commands.Left },
+                { _rightKey, Commands.Right }
+            };
+        }
+
+        private void FixedUpdate()
+        {
+            var selected = _playableActors.Where(x => x.IsSelected).ToList();
+
+            if (selected.Count() > 0)
+            {
+                selected.ForEach(x => x.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+            }
+
+            if (!Input.anyKey)
+            {
+                return;
+            }
+            for (int i = 0; i < _playableActors.Count(); ++i)
+            {
+                if (Input.GetKeyDown("" + (i + 1)))
+                {
+                    _playableActors[i].IsSelected = !_playableActors[i].IsSelected;
+                }
+            }
+
+            var pressedCommand = _keyCodesToCommands.Keys.FirstOrDefault(x => Input.GetKey(x));
+
+            if (pressedCommand != KeyCode.None)
+            {
+                _playableActors.Where(y => y.IsSelected).ToList().ForEach(x => x.GetCommand(_keyCodesToCommands[pressedCommand]));
+            }
+        }
+    }
+}
+
