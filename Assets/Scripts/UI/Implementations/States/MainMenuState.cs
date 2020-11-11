e1587c8fd8bdefd8d62688489df2697feefe0a1b
Damoch <damochramowicz260@gmail.com>
2020-11-11 14:54:59 +0100

    #6 - exit game button

--- Assets/Scripts/UI/Implementations/States/MainMenuState.cs
+++ Assets/Scripts/UI/Implementations/States/MainMenuState.cs
@@ -11,5 +11,10 @@ namespace Assets.Scripts.UI.Implementations.States
             //temporary
             SceneManager.LoadScene("test");
         }
+
+        public void ExitGameButtonClicked()
+        {
+            Application.Quit();
+        }
     }
 }
